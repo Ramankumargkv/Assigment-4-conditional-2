6e816c7438d49e7358688252c7e3972320743ec1
Raman8651 <121950276+Ramankumargkv@users.noreply.github.com>
2024-04-20 01:03:48 +0530

    Add files via upload

---
+++ Q4.cpp
@@ -0,0 +1,14 @@
+//Q4 predict the output:
+
+#include<iostream>
+using namespace std;
+int main(){
+int test = 0;
+cout << "First character " << '1' << endl;
+cout << "Second character " << (test ? 3 : '1') << endl;
+return 0;
+}
+
+// utput:- 
+// First character 1
+// Second character 49
